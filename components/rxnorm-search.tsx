c57555424d7cf211fea53abc5ef70c0ad45e5f4f
ayadbotany-hash <ayadbotany@gmail.com>
2026-09-19 06:00:59 +0300

    Update rxnorm-search.tsx

--- components/rxnorm-search.tsx
+++ components/rxnorm-search.tsx
@@ -2,133 +2,6 @@
 
 import { useState, useEffect } from "react"
 
-// مصفوفة فريدة بدون تكرار للأسماء العلمية
-const GLOBAL_GENERIC_DRUGS = Array.from(new Set([
-  // أدوية الحقن والإبر الفعالة
-  "Meropenem", "Ceftriaxone", "Cefotaxime", "Ceftazidime", "Cefepime", "Cefazolin", 
-  "Amikacin", "Gentamicin", "Tobramycin", "Vancomycin", "Teicoplanin", "Colistin",
-  "Imipenem", "Ertapenem", "Piperacillin", "Tazobactam", "Ampicillin", "Sulbactam",
-  "Metronidazole", "Ciprofloxacin", "Levofloxacin", "Moxifloxacin", "Fluconazole",
-  "Voriconazole", "Caspefungin", "Anidulafungin", "Amphotericin B", "Aciclovir",
-  
-  // الحقن المسكنة والمهدئة
-  "Diclofenac", "Ketorolac", "Piroxicam", "Paracetamol", "Tramadol", "Morphine",
-  "Fentanyl", "Pethidine", "Buprenorphine", "Ondansetron", "Metoclopramide",
-  "Hyoscine butylbromide", "Dexamethasone", "Hydrocortisone", "Methylprednisolone",
-  "Prednisolone", "Betamethasone", "Triamcinolone",
-  
-  // أدوية الأعصاب والتشنجات والقلب
-  "Levetiracetam", "Valproate sodium", "Phenytoin", "Phenobarbital", "Diazepam",
-  "Midazolam", "Lorazepam", "Haloperidol", "Amiodarone", "Atropine", "Adrenaline",
-  "Epinephrine", "Norepinephrine", "Dopamine", "Dobutamine", "Furosemide",
-  "Heparin", "Enoxaparin", "Fondaparinux", "Insulin human", "Insulin glargine",
-  
-  // الأسماء العلمية العامة (A-Z)
-  "Abacavir", "Abiraterone", "Acamprosate", "Acarbose", "Acebutolol", "Aceclofenac",
-  "Acetazolamide", "Acetylcysteine", "Acitretin", "Adalimumab", "Adapalene", "Adefovir",
-  "Adenosine", "Afatinib", "Albendazole", "Albuterol", "Alectinib", "Alendronate",
-  "Alfacalcidol", "Alfuzosin", "Aliskiren", "Allopurinol", "Alprazolam", "Alprostadil",
-  "Amantadine", "Ambrisentan", "Amiloride", "Aminophylline", "Amitriptyline", "Amlodipine",
-  "Amorolfine", "Amoxicillin", "Anagrelide", "Anastrozole", "Aripiprazole", "Artemether",
-  "Artesunate", "Ascorbic acid", "Asenapine", "Aspirin", "Atazanavir", "Atenolol",
-  "Atomoxetine", "Atorvastatin", "Azacitidine", "Azathioprine", "Azelaic acid",
-  "Azelastine", "Azithromycin", "Aztreonam", "Baclofen", "Baricitinib", "Beclomethasone",
-  "Bedaquiline", "Benazepril", "Bendamustine", "Benzoyl peroxide", "Betaxolol",
-  "Bicalutamide", "Bictegravir", "Bimatoprost", "Bisacodyl", "Bisoprolol", "Bleomycin",
-  "Bortezomib", "Bosentan", "Bosutinib", "Brimonidine", "Brinzolamide", "Brivaracetam",
-  "Bromazepam", "Bromocriptine", "Budesonide", "Bumetanide", "Bupropion", "Buspirone",
-  "Cabazitaxel", "Cabergoline", "Cabozantinib", "Calcipotriol", "Calcitonin", "Calcitriol",
-  "Calcium carbonate", "Candesartan", "Capecitabine", "Captopril", "Carbamazepine",
-  "Carbidopa", "Carbimazole", "Carboplatin", "Carfilzomib", "Carisoprodol", "Carvedilol",
-  "Cefaclor", "Cefadroxil", "Cefdinir", "Cefixime", "Cefpodoxime", "Cefprozil",
-  "Celecoxib", "Cephalexin", "Cetirizine", "Cetuximab", "Chlorambucil", "Chloramphenicol",
-  "Chlordiazepoxide", "Chlorhexidine", "Chloroquine", "Chlorpheniramine", "Chlorpromazine",
-  "Chlorthalidone", "Cholecalciferol", "Ciclesonide", "Cilostazol", "Cimetidine",
-  "Cinacalcet", "Cinnarizine", "Cisplatin", "Citalopram", "Clarithromycin", "Clindamycin",
-  "Clobetasol", "Clobazam", "Clomifene", "Clomipramine", "Clonazepam", "Clonidine",
-  "Clopidogrel", "Clotrimazole", "Clozapine", "Colchicine", "Colesevelam", "Crizotinib",
-  "Cyclobenzaprine", "Cyclophosphamide", "Cycloserine", "Cyclosporine", "Cyproheptadine",
-  "Cytarabine", "Dabigatran", "Dabrafenib", "Dacarbazine", "Daclatasvir", "Dapagliflozin",
-  "Dapoxetine", "Dapsone", "Daptomycin", "Daratumumab", "Darunavir", "Dasatinib",
-  "Daunorubicin", "Deferasirox", "Deferiprone", "Deflazacort", "Degarelix", "Denosumab",
-  "Desloratadine", "Desmopressin", "Desogestrel", "Dexmedetomidine", "Dexmethylphenidate",
-  "Dextromethorphan", "Diazoxide", "Dicyclomine", "Dienogest", "Digoxin", "Diltiazem",
-  "Dimenhydrinate", "Dimethyl fumarate", "Diphenhydramine", "Dipyridamole", "Disulfiram",
-  "Docetaxel", "Docusate", "Domperidone", "Donepezil", "Doxazosin", "Doxepin",
-  "Doxorubicin", "Doxycycline", "Dronedarone", "Drospirenone", "Duloxetine", "Dutasteride",
-  "Dydrogesterone", "Ebastine", "Econazole", "Eculizumab", "Edoxaban", "Efavirenz",
-  "Eletriptan", "Eltrombopag", "Empagliflozin", "Enalapril", "Enzalutamide", "Ephedrine",
-  "Epirubicin", "Eplerenone", "Eprosartan", "Ergocalciferol", "Ergotamine", "Erlotinib",
-  "Erythromycin", "Escitalopram", "Esmolol", "Esomeprazole", "Estradiol", "Eszopiclone",
-  "Etanercept", "Ethambutol", "Ethinylestradiol", "Ethosuximide", "Etomidate", "Etoposide",
-  "Etoricoxib", "Everolimus", "Exemestane", "Exenatide", "Ezetimibe", "Famciclovir",
-  "Famotidine", "Febuxostat", "Felodipine", "Fenofibrate", "Fenoldopam", "Ferrous sulfate",
-  "Fesoterodine", "Fexofenadine", "Filgrastim", "Finasteride", "Fingolimod", "Flecainide",
-  "Flucytosine", "Fludarabine", "Fludrocortisone", "Flumazenil", "Fluocinolone",
-  "Fluorouracil", "Fluoxetine", "Flupentixol", "Fluphenazine", "Flurazepam", "Flurbiprofen",
-  "Flutamide", "Fluticasone", "Fluvastatin", "Fluvoxamine", "Folic acid", "Formoterol",
-  "Fosinopril", "Fulvestrant", "Fusidic acid", "Gabapentin", "Galantamine", "Ganciclovir",
-  "Gefitinib", "Gemcitabine", "Gemfibrozil", "Gliclazide", "Glimepiride", "Glipizide",
-  "Glucagon", "Glycopyrrolate", "Goserelin", "Granisetron", "Griseofulvin", "Guaifenesin",
-  "Hydralazine", "Hydrochlorothiazide", "Hydromorphone", "Hydroxychloroquine",
-  "Hydroxyurea", "Hydroxyzine", "Hyoscine", "Ibrutinib", "Ibuprofen", "Ibadronic acid",
-  "Idarubicin", "Ifosfamide", "Imatinib", "Imipramine", "Imiquimod", "Infliximab",
-  "Ingenol mebutate", "Ipratropium", "Irbesartan", "Irinotecan", "Isoniazid", "Isosorbide dinitrate",
-  "Isosorbide mononitrate", "Isotretinoin", "Itraconazole", "Ivermectin", "Ivabradine",
-  "Ketoconazole", "Ketoprofen", "Ketotifen", "Labetalol", "Lacosamide", "Lactulose",
-  "Lamivudine", "Lamotrigine", "Lansoprazole", "Lapatinib", "Latanoprost", "Leflunomide",
-  "Lenvatinib", "Letrozole", "Leucovorin", "Leuprolide", "Levocetirizine", "Levonorgestrel",
-  "Levothyroxine", "Lidocaine", "Linagliptin", "Linezolid", "Liothyronine", "Liraglutide",
-  "Lisdexamfetamine", "Lisinopril", "Lithium", "Loperamide", "Lopinavir", "Loratadine",
-  "Lorlatinib", "Losartan", "Loxapine", "Magnesium sulfate", "Mannitol", "Maprotiline",
-  "Mebendazole", "Meclizine", "Medroxyprogesterone", "Mefenamic acid", "Mefloquine",
-  "Megestrol", "Meloxicam", "Melphalan", "Memantine", "Mercaptopurine", "Mesalamine",
-  "Mesna", "Metformin", "Methadone", "Methotrexate", "Methyldopa", "Methylphenidate",
-  "Metoprolol", "Mianserin", "Miconazole", "Mifepristone", "Milrinone", "Minocycline",
-  "Minoxidil", "Mirtazapine", "Misoprostol", "Mitomycin", "Mitoxantrone", "Modafinil",
-  "Moexipril", "Montelukast", "Mupirocin", "Mycophenolate mofetil", "Nabumetone",
-  "Nadolol", "Naftifine", "Naloxone", "Naltrexone", "Naproxen", "Naratriptan",
-  "Natalizumab", "Nebivolol", "Neomycin", "Neostigmine", "Nevirapine", "Nicardipine",
-  "Nicorandil", "Nicotine", "Nifedipine", "Nilotinib", "Nimodipine", "Nitrazepam",
-  "Nitrofurantoin", "Nitroglycerin", "Nitroprusside", "Norethindrone", "Norfloxacin",
-  "Nortriptyline", "Nystatin", "Octreotide", "Ofloxacin", "Olanzapine", "Olaparib",
-  "Olmesartan", "Olopatadine", "Omeprazole", "Oseltamivir", "Osimertinib", "Oxaliplatin",
-  "Oxazepam", "Oxcarbazepine", "Oxybutynin", "Oxycodone", "Oxymetazoline", "Oxytocin",
-  "Paclitaxel", "Paliperidone", "Palonosetron", "Pamidronate", "Pancuronium",
-  "Pantoprazole", "Paricalcitol", "Paroxetine", "Pazopanib", "Pembrolizumab", "Pemetrexed",
-  "Penicillamine", "Penicillin V", "Penicillin G", "Pentazocine", "Pentoxifylline",
-  "Perindopril", "Permethrin", "Perphenazine", "Phentolamine", "Phenylephrine",
-  "Physostigmine", "Pilocarpine", "Pimozide", "Pindolol", "Pioglitazone", "Piracetam",
-  "Plerixafor", "Polymyxin B", "Posaconazole", "Potassium chloride", "Prazosin",
-  "Pregabalin", "Prilocaine", "Primaquine", "Primidone", "Procarbazine", "Procyclidine",
-  "Progesterone", "Promethazine", "Propafenone", "Propofol", "Propranolol", "Propylthiouracil",
-  "Protamine", "Pyrazinamide", "Pyridostigmine", "Pyridoxine", "Pyrimethamine",
-  "Quetiapine", "Quinapril", "Quinidine", "Quinine", "Rabeprazole", "Raloxifene",
-  "Raltegravir", "Ramipril", "Ranibizumab", "Ranitidine", "Rasagiline", "Regorafenib",
-  "Remifentanil", "Repaglinide", "Reserpine", "Rifabutin", "Rifampin", "Riluzole",
-  "Risedronate", "Risperidone", "Ritonavir", "Rituximab", "Rivaroxaban", "Rivastigmine",
-  "Rizatriptan", "Rocuronium", "Ropinirole", "Ropivacaine", "Rosuvastatin", "Rupatadine",
-  "Salbutamol", "Salmeterol", "Salicylic acid", "Saxagliptin", "Scopolamine", "Secnidazole",
-  "Selegiline", "Sertaconazole", "Sertraline", "Sevelamer", "Sevoflurane", "Sildenafil",
-  "Silodosin", "Silver sulfadiazine", "Simvastatin", "Sirolimus", "Sitagliptin",
-  "Sodium valproate", "Solifenacin", "Somatropin", "Sorafenib", "Sotalol", "Spironolactone",
-  "Streptokinase", "Streptomycin", "Sucralfate", "Sufentanil", "Sulfacetamide",
-  "Sulfadiazine", "Sulfamethoxazole", "Sulfasalazine", "Sulpiride", "Sumatriptan",
-  "Sunitinib", "Tacrolimus", "Tadalafil", "Tafluprost", "Tamoxifen", "Tamsulosin",
-  "Tapentadol", "Tazarotene", "Telmisartan", "Temazepam", "Temozolomide", "Tenecteplase",
-  "Tenofovir", "Terazosin", "Terbinafine", "Terbutaline", "Teriparatide", "Testosterone",
-  "Tetrabenazine", "Tetracycline", "Thalidomide", "Theophylline", "Thiamine", "Thiopental",
-  "Thioridazine", "Ticagrelor", "Ticlopidine", "Tigecycline", "Timolol", "Tinidazole",
-  "Tiotropium", "Tizanidine", "Tocilizumab", "Tofacitinib", "Tolterodine", "Topiramate",
-  "Topotecan", "Torsemide", "Trandolapril", "Tranexamic acid", "Trastuzumab", "Travoprost",
-  "Trazodone", "Tretinoin", "Trimethoprim", "Trimipramine", "Triptorelin", "Tropicamide",
-  "Tropisetron", "Ursodeoxycholic acid", "Valacyclovir", "Valganciclovir", "Valproic acid",
-  "Valsartan", "Vardenafil", "Varenicline", "Vecuronium", "Venlafaxine", "Verapamil",
-  "Vinblastine", "Vincristine", "Vinorelbine", "Voriconazole", "Vortioxetine", "Warfarin",
-  "Zalcitabine", "Zaleplon", "Zanamivir", "Zidovudine", "Ziprasidone", "Zoledronic acid",
-  "Zolmitriptan", "Zolpidem", "Zonisamide", "Zuclopenthixol"
-]))
-
 export default function RxNormSearch({
   onSelectDrug,
   placeholder = "ابحث بالاسم العلمي (Levetiracetam, Meropenem, Pregabalin)..."
@@ -138,18 +11,38 @@ export default function RxNormSearch({
 }) {
   const [query, setQuery] = useState("")
   const [suggestions, setSuggestions] = useState<string[]>([])
+  const [dataset, setDataset] = useState<string[]>([])
 
+  // جلب البيانات من ملف public/generics.json
+  useEffect(() => {
+    async function loadGenerics() {
+      try {
+        const res = await fetch("/generics.json")
+        if (res.ok) {
+          const data = await res.json()
+          if (Array.isArray(data)) {
+            setDataset(data)
+          }
+        }
+      } catch (err) {
+        console.error("Failed to fetch generics dataset:", err)
+      }
+    }
+    loadGenerics()
+  }, [])
+
+  // التصفية الفورية
   useEffect(() => {
     if (query.trim().length >= 2) {
       const cleanQuery = query.trim().toLowerCase()
-      const matches = GLOBAL_GENERIC_DRUGS.filter(drug =>
+      const matches = dataset.filter((drug) =>
         drug.toLowerCase().includes(cleanQuery)
       )
       setSuggestions(matches)
     } else {
       setSuggestions([])
     }
-  }, [query])
+  }, [query, dataset])
 
   return (
     <div className="relative w-full max-w-md mx-auto">
